b9bdff7316e89e35006b38ff7b8db80268339d49
khoanda10 <khoanda10@gmail.com>
2022-04-28 17:23:27 +0700

    check user is logged

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: %i[new create]
+
   def new
     @user = User.new
   end
@@ -43,5 +45,12 @@ class SessionsController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = 'You are already logged in'
+      redirect_to root_path
+    end
   end
 end
