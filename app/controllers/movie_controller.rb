666304defaede77852f9daf125b76defb934069d
khoanda10 <khoanda10@gmail.com>
2022-04-28 19:14:31 +0700

    create movie into url share

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -7,6 +7,20 @@ class MovieController < ApplicationController
   end
 
   def create
-
+    if params[:movie][:url].blank?
+      flash.now[:error] = 'Please enter url'
+      render 'new'
+    else
+      movie = Movie.find_or_create_by!(url: movie_params[:url])
+      current_user.user_movies.find_or_create_by(movie_id: movie.id)
+      redirect_to root_path
+    end
   end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:url)
+  end
+
 end
