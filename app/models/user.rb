899995341c04b894a1661a66a958e67624b23e37
khoanda10 <khoanda10@gmail.com>
2022-04-28 18:15:39 +0700

    validates user resource.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, presence: true, length: { minimum: 3, maximum: 20, allow_blank: true},
+            uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 3, maximum: 20, allow_blank: true }
   has_many :user_movies
   has_many :movies, through: :user_movies
 end
